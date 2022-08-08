acc579f2721c459169908154c04d85321d2d9ca0
ThomasMoerschel <tj.moerschel@gmail.com>
2022-08-08 17:47:23 -0500

    Add project files.

---
+++ Daily_Coding_Challenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Daily_Coding_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
